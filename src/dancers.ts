5ca627cd62cb4d126926341a5ccf5779edd7f10b
Christoph Trautwein <ch.trautwein@gmail.com>
2024-04-18 11:24:52 +0200

    nicer arrows

--- src/dancers.ts
+++ src/dancers.ts
@@ -1,17 +1,26 @@
 import * as THREE from 'three';
 import { InstancedFlow } from 'three/addons/modifiers/CurveModifier.js';
+import * as BufferGeometryUtils from 'three/addons/utils/BufferGeometryUtils.js';
+
+
+function getArrow(): THREE.Mesh {
+  const material = new THREE.MeshNormalMaterial({color: 0x99ffff});
+  const coneGeom = new THREE.ConeGeometry(10, 20, 10).rotateZ(-Math.PI/2);
+  coneGeom.translate(25, 0, 0);
+  
+  const cylinderGeom = new THREE.CylinderGeometry(4, 6, 30, 10).rotateZ(-Math.PI/2);
+  // cylinder.scale(0.5, 0.5, 0.5);
+
+  const geometry=BufferGeometryUtils.mergeGeometries([coneGeom, cylinderGeom]);
+  const arrowMesh = new THREE.Mesh(geometry, material);
+  return arrowMesh;
+}
 
 export function placeDancers(scene: THREE.Scene, curves: { curve: THREE.Curve<THREE.Vector3>; }[] ): InstancedFlow {
-					const material = new THREE.MeshStandardMaterial( {
-						color: 0x99ffff
-					} );
-
+					const arrow = getArrow();
 					const numberOfInstances = 8;
-                    // const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-                    const geometry = new THREE.ConeGeometry( 20, 50, 8 ).rotateZ(-Math.PI/2); 
-                    // const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-					// flow = new InstancedFlow( numberOfInstances, curves.length, geometry, material );
-					const flow = new InstancedFlow( numberOfInstances, curves.length, geometry, material );
+
+					const flow = new InstancedFlow( numberOfInstances, curves.length, arrow.geometry,<THREE.Material>arrow.material );
 
 					curves.forEach( function ( { curve }, i ) {
 						flow.updateCurve( i, curve );
